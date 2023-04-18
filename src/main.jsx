4a3faa85c01118b6a1312cd619ff0a75e0c5b1d2
Andre L. G. Sampaio <andreluizsampa@gmail.com>
2023-04-18 17:04:20 -0300

    Reorganize project to my linkings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { AppRoutes } from "./routes"
+import theme from './styles/theme'
+import GlobalStyle from './styles/global'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <AppRoutes/>
+    </ThemeProvider>  
   </React.StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export default{
+    light:{
+        titles: "#00FF00"
+    },
+
+    dark:{
+        titles: "#004400"
+    }
+}
